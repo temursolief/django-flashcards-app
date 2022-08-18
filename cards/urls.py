de4d2c5610a4aaf5a460c43d670d7d599907d155
Temurbek Soliev <temursoliev2110@gmail.com>
2022-08-19 01:33:58 +0500

    fix: urls and view in cards app as well as new templates for creating cards

--- cards/urls.py
+++ cards/urls.py
@@ -1,7 +1,13 @@
 from re import template
 from django.urls import path
-from .views import CardListView
+from .views import (
+    CardListView, 
+    CardCreateView, 
+    CardUpdateView
+)
 
 urlpatterns = [
-    path("", CardListView.as_view(), name="card-list")
+    path("", CardListView.as_view(), name="card-list"),
+    path('new', CardCreateView.as_view(), name="card-create"),
+    path("edit/<int:pk>/", CardUpdateView.as_view(), name="card-update")
 ]
