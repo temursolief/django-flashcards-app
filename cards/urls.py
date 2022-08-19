4e84414e7d719e009beb84713732298a50dccc5a
Temurbek Soliev <temursoliev2110@gmail.com>
2022-08-19 11:51:54 +0500

    feat:added forms and templates

--- cards/urls.py
+++ cards/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import (
     CardListView, 
     CardCreateView, 
-    CardUpdateView
+    CardUpdateView,
+    BoxView,
 )
 
 urlpatterns = [
     path("", CardListView.as_view(), name="card-list"),
     path('new', CardCreateView.as_view(), name="card-create"),
-    path("edit/<int:pk>/", CardUpdateView.as_view(), name="card-update")
+    path("edit/<int:pk>/", CardUpdateView.as_view(), name="card-update"),
+    path('box/<int:box_num>/', BoxView.as_view(), name='box'),
 ]
